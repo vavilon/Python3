14f17df85016c525bfdf329755135f998efdd918
vavilon <overon4ek@gmail.com>
2017-09-24 15:48:51 +0300

    23_09_2017

--- django/bootstrap_news/news/models.py
+++ django/bootstrap_news/news/models.py
@@ -7,11 +7,12 @@ class Post(models.Model):
     author = models.ForeignKey('auth.User')
     title = models.CharField(max_length=200)
     text = models.TextField()
+    pre_Text = models.TextField()
     create_date = models.DateTimeField(
         default=timezone.now)
     published_date = models.DateTimeField(
         blank=True, null=True)
-    
+
     def publish(self):
         self.published_date = timezone.now()
         self.save()
